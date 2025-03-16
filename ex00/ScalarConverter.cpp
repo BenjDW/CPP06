79efdd4a2783af0991b1a76302e304c740991f55
BenjDW <dewitskibenji@live.fr>
2025-03-16 08:04:38 -0700

    start the int conversions

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,18 +6,20 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 06:25:13 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/15 07:38:06 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/16 08:03:06 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter()
+ScalarConverter::ScalarConverter() : imput("42")
 {
-
 }
 
-ScalarConverter::ScalarConverter(ScalarConverter &cpy)
+ScalarConverter::ScalarConverter(std::string in) : imput(in)
+{}
+
+ScalarConverter::ScalarConverter(ScalarConverter &cpy) : imput(cpy.imput)
 {
 
 }
@@ -27,11 +29,48 @@ ScalarConverter::~ScalarConverter()
 
 ScalarConverter&	ScalarConverter::operator=(ScalarConverter const &cpy)
 {
-
+	this->imput = cpy.imput;
+	return *this;
 }
 
+// Write a class ScalarConverter that will contain only one static method "convert"
+// that will take as a parameter a string representation of a C++ literal in its most common
+// form and output its value in the following series of scalar types:
+// • char
+// • int
+// • float
+// • double
+// As this class doesn’t need to store anything at all, it must not be instantiable by users.
+// Except for char parameters, only the decimal notation will be used.
+// Examples of char literals: ’c’, ’a’, ...
+// To make things simple, please note that non-displayable characters shouldn’t be used as
+// inputs. If a conversion to char is not displayable, print an informative message.
+// Examples of int literals: 0, -42, 42...
+// Examples of float literals: 0.0f, -4.2f, 4.2f...
+// You have to handle these pseudo-literals as well (you know, for science): -inff, +inff,
+// and nanf.
+
+// need to refactor with this->imput;
+// static_cast<type>(variable)
 void ScalarConverter::convert(std::string base)
 {
+	long long	temp;
 
+	if (base.empty() == true)
+	{
+		std::cout << "Char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
+	}
+	else
+	{
+		temp = std::stoi(base, nullptr, 10);
+		if (temp == NULL)
+			std::cout << "int: impossible" << std::endl;
+		else if (temp < -2147483648 || temp > 2147483647)
+			std::cout << "int: impossible" << std::endl;
+		else
+			std::cout << "int:" << static_cast<int>(temp) << std::endl;
+	}
 }
-
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    ScalarConverter.hpp                               :+:      :+:    :+:   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 05:17:19 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/15 07:29:40 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/15 07:56:35 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,12 @@ class	ScalarConverter
 {
 	public:
 		ScalarConverter();
+		ScalarConverter(std::string in);
 		ScalarConverter(ScalarConverter &cpy);
 		~ScalarConverter();
 		ScalarConverter &operator=(ScalarConverter const &cpy);
 		static void convert(std::string base);
-		
+		std::string	imput;
 	private:
 
 };
